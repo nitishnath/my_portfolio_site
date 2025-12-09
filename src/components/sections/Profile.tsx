a3ccc92caa1185b5c4ce59e19c14ff88e0c1c447
Nitish <nathnitish157@gmail.com>
2025-12-09 23:04:28 +0530

    updated code

--- src/components/sections/Profile.tsx
+++ src/components/sections/Profile.tsx
@@ -5,7 +5,7 @@ import AboutResumePanel from "@/components/sections/AboutResumePanel";
 
 export default function Profile() {
   return (
-    <section className="mx-auto max-w-7xl px-8 py-10">
+    <section className="mx-auto bg-zinc-800 px-8 py-10 shadow-xl ring-1 ring-white/10">
       <div className="grid gap-8 lg:grid-cols-[406px_1fr]">
         <SidebarProfile />
         <AboutResumePanel />
